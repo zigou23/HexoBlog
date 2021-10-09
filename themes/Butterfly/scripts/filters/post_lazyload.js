d0794be5af83f1d4e62cd228f48f9840914364ed
zigou23 <a1923458037@gmail.com>
2021-10-09 22:25:58 +0800

    init commit

--- themes/Butterfly/scripts/filters/post_lazyload.js
+++ themes/Butterfly/scripts/filters/post_lazyload.js
@@ -1,22 +1,29 @@
 /**
  * Butterfly
  * lazyload
- * replace src to data-src
+ * replace src to data-lazy-src
  */
 
 'use strict'
 
 const urlFor = require('hexo-util').url_for.bind(hexo)
 
-function lazyProcess (htmlContent) {
-  var bg = hexo.theme.config.lodding_bg.post ? urlFor(hexo.theme.config.lodding_bg.post) : 'data:image/gif;base64,R0lGODdhAQABAPAAAMPDwwAAACwAAAAAAQABAAACAkQBADs='
-  return htmlContent.replace(/(<img .*?src=)/ig, `$1 ${bg} data-src=`)
+function lazyload (htmlContent) {
+  const bg = hexo.theme.config.lazyload.placeholder ? urlFor(hexo.theme.config.lazyload.placeholder) : 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
+  return htmlContent.replace(/(<img.*? src=)/ig, `$1 "${bg}" data-lazy-src=`)
 }
 
-var processPost = function (data) {
-  if (!hexo.theme.config.lazyload) return
-  data.content = lazyProcess.call(this, data.content)
+hexo.extend.filter.register('after_render:html', function (data) {
+  const config = hexo.theme.config.lazyload
+  if (!config.enable) return
+  if (config.field !== 'site') return
+  return lazyload.call(this, data)
+})
+
+hexo.extend.filter.register('after_post_render', data => {
+  const config = hexo.theme.config.lazyload
+  if (!config.enable) return
+  if (config.field !== 'post') return
+  data.content = lazyload.call(this, data.content)
   return data
-}
-
-hexo.extend.filter.register('after_post_render', processPost)
+})
